c9f6dc2140242d76059cc6d0eace6775075a82c8
Sasitha Lakshan <74831645+SasithaLakshan@users.noreply.github.com>
2024-04-05 19:13:13 +0530

    Button Components

---
+++ src/components/colsutationButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './colsutationButton.scss'
+
+const ConsultationButton = () =>{
+    return(
+        <button>Get Free Consultation</button>
+    )
+}
+
+export default ConsultationButton;
---
+++ src/components/learnMore.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './learnMore.scss';
+
+const LearnMoreButton = () =>{
+    return(
+        <button>Learn More</button>
+    )
+}
+
+export default LearnMoreButton;
