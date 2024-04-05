ce32e952ea9436c53cd5dbf0b4da8ef659853e82
Sasitha Lakshan <74831645+SasithaLakshan@users.noreply.github.com>
2024-04-05 15:13:33 +0530

    Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.scss'
 import Header from './components/header'
+import Footer from './components/footer'
 import heroImage from './assets/Hero.png'
 
 function App() {
@@ -7,13 +8,23 @@ function App() {
     <>
     <Header/>
     <main>
-      <section className='hero_section'>
+      <section id='hero_section'>
         <h1>Hero Section</h1>
       </section>
-      <section className='other_section'>
-        <h1>Other Section</h1>
+      <section id='services_section'>
+        <h1>Services Section</h1>
+      </section>
+      <section id='about_section'>
+        <h1>About Section</h1>
+      </section>
+      <section id='contact_section'>
+        <h1>Contact Section</h1>
+      </section>
+      <section id='career_section'>
+        <h1>Career Section</h1>
       </section>
     </main>
+    <Footer/>
     </>
   )
 }
